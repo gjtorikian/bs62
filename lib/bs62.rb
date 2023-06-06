f823abae5ea79be4fa3001e37f6b2d4b77a6af6c
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2023-06-07 00:04:22 +0200

    it lives

---
+++ lib/bs62.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bs62/version"
+
+module Bs62
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "bs62"
+
+require "minitest/autorun"
