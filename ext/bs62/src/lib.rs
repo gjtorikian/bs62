b3d9fbb95d283151a14972888c948a173f418763
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2025-12-19 12:10:34 -0500

    fix deprecations

--- ext/bs62/src/lib.rs
+++ ext/bs62/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate core;
 
 use base_62::base62;
-use magnus::{define_class, exception, function, Error, Object};
+use magnus::{function, Error, Object, Ruby};
 
 #[magnus::wrap(class = "Bs62")]
 struct Bs62 {}
@@ -13,17 +13,20 @@ impl Bs62 {
     pub fn perform_decode(rb_str: String) -> Result<Vec<u8>, Error> {
         match base62::decode(rb_str.as_str()) {
             Ok(v) => Ok(v),
-            Err(e) => Err(magnus::Error::new(
-                exception::runtime_error(),
-                format!("Could not call decode: {:?}", e),
-            )),
+            Err(e) => {
+                let ruby = Ruby::get().unwrap();
+                Err(magnus::Error::new(
+                    ruby.exception_runtime_error(),
+                    format!("Could not call decode: {:?}", e),
+                ))
+            }
         }
     }
 }
 
 #[magnus::init]
-fn init() -> Result<(), Error> {
-    let c_bs62 = define_class("Bs62", magnus::class::object())?;
+fn init(ruby: &Ruby) -> Result<(), Error> {
+    let c_bs62 = ruby.define_class("Bs62", ruby.class_object())?;
 
     c_bs62.define_singleton_method("perform_encode", function!(Bs62::perform_encode, 1))?;
     c_bs62.define_singleton_method("perform_decode", function!(Bs62::perform_decode, 1))?;
